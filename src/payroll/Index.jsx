3d79f469ff19c9bf7b093d8f9421d4ae4249a36f
Ghazanfar Abbas <116085344+GhazanfarGw@users.noreply.github.com>
2025-08-17 00:44:36 +0500

    Update Index.jsx

--- src/payroll/Index.jsx
+++ src/payroll/Index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Dashboard from "./Dashboard"
+import Price from "./Price"
 import Anywhere from "./Anywhere"
 import Subr from "./Subr"
 import About from "./About"
@@ -12,9 +13,10 @@ export default function Index() {
     <>
     <Header/>
     <Dashboard/>
+    <Price/>
     <Anywhere/>
-    <About/>
     <Slider/>
+    <About/>
     <Subr/>
     <Footer/>
     </>
